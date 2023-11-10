d77adf3e780a3a1c7cd1442439ea2d65337c7c4c
Dorian Marié <dorian@thoughtbot.com>
2023-11-10 11:43:13 +0100

    add test and Gemfile.lock

---
+++ spec/language_spec.rb
@@ -0,0 +1,19 @@
+class Code
+  class Parser
+    class Nothing < Language
+      def nothing_keyword
+        str("nothing")
+      end
+
+      def root
+        nothing_keyword.aka(:nothing)
+      end
+    end
+  end
+end
+
+RSpec.describe Language do
+  it "works" do
+    expect(Code::Parser::Nothing.parse("nothing")).to eq({ nothing: "nothing" })
+  end
+end
