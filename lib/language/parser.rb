55bbe8cd995bc3a883adfdb4dfa7b6f41fe68ed5
Dorian Marié <dorian@dorianmarie.com>
2024-08-29 21:51:56 +0200

    refactor output handling logic

--- lib/language/parser.rb
+++ lib/language/parser.rb
@@ -19,7 +19,11 @@ class Language
         raise NotEndOfInput, self
       end
 
-      @output.present? ? @output : Output.new(@buffer.empty? ? nil : @buffer)
+      if @output.present?
+        @output
+      else
+        Output.new(@buffer.empty? ? nil : @buffer)
+      end
     end
 
     def consume(n)
